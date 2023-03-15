73391aabbd55977b54d5bd7da26fcf54b1468b23
IT21261350 <89385432+IT21261350@users.noreply.github.com>
2023-03-16 01:09:13 +0530

    changing class name

--- app/src/main/java/com/example/fixit/LogInSignUp.kt
+++ app/src/main/java/com/example/fixit/LogInSignUp.kt
@@ -3,9 +3,9 @@ package com.example.fixit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LogInSignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_signup)
     }
 }
