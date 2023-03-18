3fbe1a9fc7567883b6b025d4cedeb4c6de65c0d9
Shmary Shiraz <shamrishiraz01@gmail.com>
2023-03-19 00:21:31 +0530

    Customer Home and miner changes with the map

--- app/src/main/java/com/example/fixit/Scanner.kt
+++ app/src/main/java/com/example/fixit/Scanner.kt
@@ -3,12 +3,9 @@ package com.example.fixit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CustomerHome : AppCompatActivity() {
+class Scanner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer_home)
-
-
-
+        setContentView(R.layout.activity_scanner)
     }
 }
