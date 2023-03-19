3a1c74c72e45a35229ad7ae457bffd1288ab06d5
Neranjan Dissanayake <it17100458@my.sliit.lk>
2023-03-19 16:22:29 +0530

    Wallet function updated

---
+++ app/src/main/java/com/example/fixit/TransactionSummery.kt
@@ -0,0 +1,11 @@
+package com.example.fixit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TransactionSummery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction_summery)
+    }
+}
