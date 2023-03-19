5e21ba86529d48103c4490a192f0ae8245d3f54a
Neranjan Dissanayake <it17100458@my.sliit.lk>
2023-03-19 11:39:19 +0530

    Updated review function

---
+++ app/src/main/java/com/example/fixit/ConfirmAlert.kt
@@ -0,0 +1,11 @@
+package com.example.fixit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmAlert : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_alert)
+    }
+}
