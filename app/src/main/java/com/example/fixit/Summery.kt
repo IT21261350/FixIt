e0384924b3b9f07517c8d40b506bd74bcbf8427a
Neranjan Dissanayake <it17100458@my.sliit.lk>
2023-05-12 15:24:26 +0530

    Wallet function updated

--- app/src/main/java/com/example/fixit/Summery.kt
+++ app/src/main/java/com/example/fixit/Summery.kt
@@ -1,11 +1,11 @@
 package com.example.fixit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class AlertBox : AppCompatActivity() {
+class Summery : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alert_box)
+        setContentView(R.layout.summery)
     }
 }
