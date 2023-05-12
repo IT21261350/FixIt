97e5b873d7da13ee20037988504d55584dd99535
IT17100458 <it17100458@my.sliit.lk>
2023-05-13 00:17:37 +0530

    added add review activity code

--- app/src/main/java/com/example/fixit/AddReview.kt
+++ app/src/main/java/com/example/fixit/AddReview.kt
@@ -10,7 +10,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fixit.model.ReviewData
 import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.lang.Exception
@@ -22,14 +25,38 @@ class AddReview : AppCompatActivity() {
                 setContentView(R.layout.activity_review)
                 FirebaseApp.initializeApp(this)
 
+
+                //Testing Connection to firebase server
+                val testDatabase = FirebaseDatabase.getInstance()
+                val connectedRef = testDatabase.getReference(".info/connected")
+
+                connectedRef.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                                val connected = snapshot.getValue(Boolean::class.java) ?: false
+                                if (connected) {
+                                        // Client is connected to Firebase server
+                                        Log.d("Add Review Activity", "Client is connected to Firebase server")
+                                } else {
+                                        // Client is not connected to Firebase server
+                                        Log.d("Add Review Activity", "Client is not connected to Firebase server")
+                                }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                                // Failed to read value
+                                Log.d("Add Review Activity", "Failed to read value connection cancelled")
+                        }
+                })
+
+
                 var database = Firebase.database.reference
 
                 val nameTextField = findViewById<EditText>(R.id.nameTextField)
                 val emailTextField = findViewById<EditText>(R.id.emailTextField)
                 val reviewTitleTextField = findViewById<EditText>(R.id.reviewTitleTextField)
                 val reviewDescriptionTextFiled = findViewById<EditText>(R.id.reviewDescriptionTitleField)
-                var ratingsBar = findViewById<RatingBar>(R.id.ratingBar)
-                var submitButton = findViewById<Button>(R.id.submitButton)
+                var ratingsBar = findViewById<RatingBar>(R.id.ratingBar_view)
+                var submitButton = findViewById<Button>(R.id.saveChangesButton)
 
                 Log.d("Add Review Activity", "On Create Activity")
 
@@ -47,12 +74,12 @@ class AddReview : AppCompatActivity() {
 
                         try {
                                 database
-                                        .child("reviews")
+                                        .child("reviews").child(name)
                                         .setValue(review)
                                         .addOnSuccessListener {
                                                 Log.d("Add Review Activity", "DB Works")
                                                 val intent = Intent(this, ReviewProfile::class.java)
-                                                intent.putExtra("added_review", review.toString())
+                                                intent.putExtra("added_review", name)
                                                 startActivity(intent)
                                         }.addOnFailureListener {
                                                 Log.d("Add Review Activity", "DB fail")
